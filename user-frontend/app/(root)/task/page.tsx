dbcacefcfafe97143e5e4bec4fc3f273be7071fa
Bhanavi Goyal <bhanavigoyal@gmail.com>
2025-01-28 12:23:13 +0530

    added task page

---
+++ user-frontend/app/(root)/task/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    // map all the tasks by the wallet here and if the user does not has any task return no tasks
+    // give the option to click on any task and see that
+    return <div>
+        all tasks
+        
+    </div>
+}
